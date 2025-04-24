25567880fa0eac85cabf3466ff51cd8d02c8a3e0
ohshitnotgood <praanto@icloud.com>
2025-04-24 14:23:43 +0200

    updated v1 forms for both endpoints

--- src/routes/v1/v1/json/+page.svelte
+++ src/routes/v1/v1/json/+page.svelte
@@ -11,7 +11,7 @@
 
         printableQuery = JSON.stringify(data)
         
-        const apiUrl = "https://thesis-test-server.vercel.app/v2/json"
+        const apiUrl = "https://thesis-test-server.vercel.app/v1/json"
         const response = await fetch(apiUrl, {
             method: "PUT",
             headers: {
@@ -60,37 +60,21 @@
                     >Password</label
                 >
             </div>
-            <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="grid md:grid-cols-1 md:gap-6">
                 <div class="relative z-0 w-full mb-5 group">
                     <input
                         type="text"
-                        name="first_name"
-                        defaultValue="Praanto"
-                        id="first_name"
+                        name="full_name"
+                        defaultValue="Praanto Samadder"
+                        id="full_name"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        placeholder=" "
+                        placeholder=""
                         required
                     />
                     <label
-                        for="first_name"
+                        for="full_name"
                         class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                        >First name</label
-                    >
-                </div>
-                <div class="relative z-0 w-full mb-5 group">
-                    <input
-                        type="text"
-                        name="last_name"
-                        defaultValue="Samadder"
-                        id="last_name"
-                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        placeholder=" "
-                        required
-                    />
-                    <label
-                        for="last_name"
-                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                        >Last name</label
+                        >Full name</label
                     >
                 </div>
             </div>
@@ -115,9 +99,9 @@
                 <div class="relative z-0 w-full mb-5 group">
                     <input
                         type="text"
-                        name="company"
-                        id="company"
-                        defaultValue="Outpost24"
+                        name="username"
+                        id="username"
+                        defaultValue="ohshitnotgood"
                         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" "
                         required
